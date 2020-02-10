405b9a27fee05d101e1fb22b8a5fccc5e4dd2822
alexarmatas <alexarmatas97@gmail.com>
2020-02-10 10:43:45 -0500

    added opacity transition to nonselected

--- onClick.js
+++ onClick.js
@@ -1,19 +1,31 @@
 //declare all things I need on the DOM
 const IMAGES = document.querySelectorAll("img");
+const IMAGETEXT = document.querySelectorAll(".imgText");
+const STARTTEXT = document.querySelector(".startText");
 
 const imageArray = Array.from(IMAGES);
+const imageTextArray = Array.from(IMAGETEXT);
 
-function removableFunction(){
+function onListener(){
         const selectedImage = event.target;
         const nonSelected = imageArray.filter((image) => image != event.target);
-        nonSelected.forEach( function(img){
-
-            img.style.opacity = "0";
+        nonSelected.forEach( img => img.style.opacity ="0"); 
+        imageTextArray.forEach(function(txt){
+            txt.style.transition ="2s";
+            txt.style.opacity ="0";
         });
-        IMAGES.forEach( img => img.removeEventListener('click', removableFunction));
+        STARTTEXT.style.transition = "2s";
+        STARTTEXT.style.opacity = "0";
+        IMAGES.forEach( img => img.removeEventListener('click', onListener));
+
     }
 
-IMAGES.forEach( img => img.addEventListener('click', removableFunction));
+IMAGES.forEach( img => img.addEventListener('click', onListener));
+
+
+//Transition into the game start scene
+
+
 
 
 
