6b3679cbd3a0d044200c68231f5945d4c268866d
alexarmatas <alexarmatas97@gmail.com>
2020-02-11 10:10:57 -0500

    working on removing child nodes after animation

--- onClick.js
+++ onClick.js
@@ -1,4 +1,5 @@
 //declare all things I need on the DOM
+
 const IMAGES = document.querySelectorAll("img");
 const IMAGETEXT = document.querySelectorAll(".imgText");
 const STARTTEXT = document.querySelector(".startText");
@@ -6,22 +7,38 @@ const STARTTEXT = document.querySelector(".startText");
 const imageArray = Array.from(IMAGES);
 const imageTextArray = Array.from(IMAGETEXT);
 
+function opacityTransition(nonSelected){
+    console.log();
+    nonSelected.forEach( img => img.style.opacity ="0"); 
+    imageTextArray.forEach(function(txt){
+        txt.style.transition ="1s";
+        txt.style.opacity ="0";
+    });
+    STARTTEXT.style.transition = "1s";
+    STARTTEXT.style.opacity = "0";
+    IMAGES.forEach( img => img.removeEventListener('click', onListener));
+}
+
+function removeChildNode(nonSelected){
+    for(let i = 0; i < nonSelected.length; i++){;
+        let parent = nonSelected[i].parentNode;
+        parent.removeChild(nonSelected[i]);
+    }
+
+}
+
+
 function onListener(){
         const selectedImage = event.target;
         const nonSelected = imageArray.filter((image) => image != event.target);
-        nonSelected.forEach( img => img.style.opacity ="0"); 
-        imageTextArray.forEach(function(txt){
-            txt.style.transition ="2s";
-            txt.style.opacity ="0";
-        });
-        STARTTEXT.style.transition = "2s";
-        STARTTEXT.style.opacity = "0";
-        IMAGES.forEach( img => img.removeEventListener('click', onListener));
-
-    }
+        opacityTransition(nonSelected);
+        removeChildNode(nonSelected);
+        
+}
 
 IMAGES.forEach( img => img.addEventListener('click', onListener));
 
+//remove other backgrounds files and then transition
 
 //Transition into the game start scene
 
