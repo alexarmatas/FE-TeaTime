993530a2a1b8815a0859d028a12612a08a6b5167
alexarmatas <alexarmatas97@gmail.com>
2020-02-17 10:48:44 -0500

    Began working on game engine

--- onClick.js
+++ onClick.js
@@ -3,54 +3,57 @@ const imageArray = Array.from(IMAGES);
 const hearts_p = document.querySelectorAll(".scoreText");
 const questions_p = document.querySelectorAll(".gameText");
 var gameCharacter = "";
-var gameImage = "";
+var game_img = document.querySelector("#gameImage");
+
 const goodAnswers = [];
 const neutralAnswers = [];
 const badAnswers = [];
+const maxRounds = 5;
 
 
-//Game Start 
-IMAGES.forEach( img => img.addEventListener('click', onListener));
 
-function onListener(){
-        let eventTarget = event.target;
-        gameSet(eventTarget);
-        gameStart();
-        //our main image won't load until our function finishes!!!
 
+//HomePage Functions
+
+
+const listener = (event) => {
+    let eventTarget = event.target;
+    gameSet(eventTarget); 
+} 
+
+
+
+   
+function addListeners(){
+    IMAGES.forEach( img => img.addEventListener('click', listener));
 }
 
 function gameSet(target){
         gameCharacter = (target.nextSibling).nextSibling.innerText;
         gameCharacter = gameCharacter.toLowerCase();
-        localStorage.setItem("gameCharacter", gameCharacter)
-        gameCharacter = localStorage.getItem("gameCharacter");
+        localStorage.setItem("gameCharacter", gameCharacter);
         window.location.href = "gameStart.html";
-
+  
 }
 
-
-
-function gameStart(){
-    console.log(document.images)
-}
-
-
-
+//GamePage 
+//Make the characterRoute function start on the new page
 
 
 //Game Engine
 
-function characterRoute(character){
-    switch(character){
+
+function characterRoute(){
+    gameCharacter = localStorage.getItem("gameCharacter");
+    switch(gameCharacter){
         case "azura":
-            console.log(azura.images.base);
-            document.getElementById('gameImage') = azura.images.base;
+            characterGame(azura);
+            break;
 
         case "anna":
-            console.log(anna.images.base);
-            avatar_img.src = anna.images.base;
-
+            characterGame(anna);
+            break;
+   
         //case forrest:
 
         //case leo:
@@ -60,6 +63,55 @@ function characterRoute(character){
         //case odin:
     }
 
+}
+
+//draw the answers 
+
+//get player input that returns values 
+
+function random(length){
+    let randomValue = Math.floor(Math.random() * length);
+    return randomValue;
+}
+
+function characterGame(character){
+    game_img.src = character.images.base;
+    let roundNumber = 0;
+    let playerScore = 0;
+    //inside the loop
+    let goodChoice = character.chatOptions.good.splice(random(character.chatOptions.good.length),1);
+    let neutralChoice = character.chatOptions.neutral.splice(random(character.chatOptions.neutral.length),1);
+    let badChoice = character.chatOptions.bad.splice(random(character.chatOptions.bad.length),1);
+    let choiceArray = [goodChoice, neutralChoice, badChoice];
+    let passArray = [goodChoice, neutralChoice, badChoice];
+    let drawArray = [];
+    
+    for(let i=0; i < 3; i++){
+        drawArray.push(choiceArray.splice(random(choiceArray.length), 1));
+    }
+    for(let i=0; i < 3; i++){
+        questions_p[i].innerHTML = drawArray[i];
+        questions_p[i].addEventListener('click', (event) =>{
+            let eventTarget = event.target;
+            console.log(typeof eventTarget.innerHTML);
+            console.log(typeof passArray[0]);
+            switch(eventTarget.innerText){
+                case passArray[0]:
+                    console.log("win");
+                    break;
+
+                case passArray[1]:
+                    console.log("neutral");
+                    break;
+
+                case passArray[2]:
+                    console.log("lose");
+                    break;
+            }
+
+        });
+    }
+ 
 }
 
 
@@ -94,10 +146,10 @@ class Character{
 
 
 const azura = new Character('azura', new charImages('images/azura/azuraBase.png', 'images/azura/azuraSmile.png', 'images/azura/azuraFrown.png') ,
-  new ChatOptions(['0','1','2','3','4','5'], ['6','7','8','9','10'], ['11','12','13','14','15']));
+  new ChatOptions(['a','b','c','d','e','f'], ['g','h','i','j','k','l'], ['m','n','o','p','q','r']));
 
 const anna = new Character('azura', new charImages('images/anna/annaBase.png', 'images/anna/annaSmile.png', 'images/anna/annaFrown.png'),
-  new ChatOptions(['0','1','2','3','4','5'], ['6','7','8','9','10'], ['11','12','13','14','15']));
+  new ChatOptions(['0','1','2','3','4','5'], ['6','7','8','9','10','10'], ['11','12','13','14','15','16']));
 
  
 
